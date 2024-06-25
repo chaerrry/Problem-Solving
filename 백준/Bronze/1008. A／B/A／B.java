9afb0655d0f8182e2b54057c4eefa738909b6e7d
박채연 <98946224+chaerrry@users.noreply.github.com>
2024-06-25 23:18:31 +0900

    [Bronze V] Title: A/B, Time: 216 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        
+        double C = (double)A/B;
+        System.out.print(C);
+    }
+}
