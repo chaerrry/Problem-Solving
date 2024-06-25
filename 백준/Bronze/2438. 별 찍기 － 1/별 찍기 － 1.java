300b645eb3747af6f6c5166b88950461bb334eb4
박채연 <98946224+chaerrry@users.noreply.github.com>
2024-06-25 23:49:40 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 248 ms, Memory: 18128 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        
+        for(int i = 0; i < a; i++){
+            for(int j = 0; j < (i+1); j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
