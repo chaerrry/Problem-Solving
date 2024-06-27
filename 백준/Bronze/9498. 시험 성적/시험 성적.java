11bfa317f95dea39ef70adab1681d1564cae0f13
박채연 <98946224+chaerrry@users.noreply.github.com>
2024-06-27 23:46:19 +0900

    [Bronze V] Title: 시험 성적, Time: 184 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        
+        int a = scan.nextInt();
+        if(90<=a && a<=100){
+            System.out.println("A");
+        }else if(80<=a){
+            System.out.println("B");
+        }else if(70<=a){
+            System.out.println("C");
+        }else if(60<=a){
+            System.out.println("D");
+        }else {
+            System.out.println("F");
+        }
+    }
+}
