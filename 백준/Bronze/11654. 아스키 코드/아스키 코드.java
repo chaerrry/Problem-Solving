a104602f40fff1b55db963a5814b863fbcc55cf5
박채연 <98946224+chaerrry@users.noreply.github.com>
2024-07-03 22:02:46 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner =new Scanner(System.in);
+        String s = scanner.next();
+        char ch = s.charAt(0);
+        int i = ch;
+        System.out.println(i);
+    }
+
+}
