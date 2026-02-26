7d7cf87f93dc97276b9e9b3ba485013d6083c54a
박채연 <98946224+chaerrry@users.noreply.github.com>
2026-02-27 04:46:35 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 11.59 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -7,10 +7,11 @@ class Solution {
         str1 = str1.toLowerCase();
         str2 = str2.toLowerCase();
 
-        // 두 글자씩 끊어서 리스트 만들기
-        ArrayList<String> A = new ArrayList<>();
-        ArrayList<String> B = new ArrayList<>();
-
+        // 두 글자씩 끊어서 Map
+        Map<String, Integer> map1 = new HashMap<>();
+        Map<String, Integer> map2 = new HashMap<>();
+        
+        
         // A 자르기
         for (int i = 0; i < str1.length() - 1; i++) {
             char c1 = str1.charAt(i);
@@ -18,44 +19,68 @@ class Solution {
 
             // 영문자만 유효
             if (Character.isLetter(c1) && Character.isLetter(c2)) {
-                A.add("" + c1 + c2);
+                //A.add("" + c1 + c2);
+                String compare = "" + c1 + c2;
+                // 문자로 만든 후 map에 바로 비교후 value값에 갯수
+                if(map1.containsKey(compare)){
+                    map1.put(compare, map1.get(compare) + 1);
+                }else {
+                    map1.put(compare, 1);
+                }
             }
         }
 
-        // str2 자르기
+        // B 자르기
         for (int i = 0; i < str2.length() - 1; i++) {
             char c1 = str2.charAt(i);
             char c2 = str2.charAt(i + 1);
 
+            // 영문자만 유효
             if (Character.isLetter(c1) && Character.isLetter(c2)) {
-                B.add("" + c1 + c2);
+                //A.add("" + c1 + c2);
+                String compare = "" + c1 + c2;
+                // 문자로 만든 후 map에 바로 비교후 value값에 갯수
+                if(map2.containsKey(compare)){
+                    map2.put(compare, map2.get(compare) + 1);
+                }else {
+                    map2.put(compare, 1);
+                }
             }
         }
 
         // 둘 다 공집합이면 65536 반환
-        if (A.size() == 0 && B.size() == 0) {
+        //if (A.size() == 0 && B.size() == 0) {
+        // map은 empty 사용
+        if (map1.isEmpty() && map2.isEmpty()) {
             return 65536;
         }
 
         // 교집합
-        boolean[] used = new boolean[B.size()];
-        int result = 0;
+        int min = 0;
+        int max = 0;
 
-        for (int i = 0; i < A.size(); i++) {
-
-            for (int j = 0; j < B.size(); j++) {
-                if (!used[j] && A.get(i).equals(B.get(j))) {
-                    used[j] = true;
-                    result++;
-                    break;  
-                }
+        for(String key : map1.keySet()){
+            int cnt1 = map1.get(key);
+            
+            if(map2.containsKey(key)){
+                int cnt2 = map2.get(key);
+                
+                min += Math.min(cnt1, cnt2);
+                max += Math.max(cnt1, cnt2);
+            }else{
+                max += cnt1;
             }
         }
-        // 합집합
-        int result2 = A.size() + B.size() - result;
+        
+        // b에만 있는값 찾기
+        for(String key : map2.keySet()){
+            if(!map1.containsKey(key)){
+                max += map2.get(key);
+            }
+        }
+        
+        double result = (double) min / max;
 
-        double result3 = (double) result / result2;
-
-        return (int) (result3 * 65536);
+        return (int) (result * 65536);
     }
 }
