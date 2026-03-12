7f990a7f2646c1b1c99b42fb70348ece94d3d369
박채연 <98946224+chaerrry@users.noreply.github.com>
2026-03-12 23:17:31 +0900

    [level 2] Title: 더 맵게, Time: 1499.77 ms, Memory: 124 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        // 우선순위큐 생성
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        int answer = 0;
+        
+        // 큐에 배열 담기
+        for(int i : scoville){
+            minHeap.offer(i);
+        }
+        
+        for(answer = 0; minHeap.size() > 1; answer++){    
+            int a = minHeap.poll();
+            if(a >= K){
+                return answer;
+            }
+            int b = minHeap.poll() * 2;   
+            minHeap.offer(a+b);
+        }
+        
+        if(minHeap.peek() >= K){
+            return answer;
+        }else {
+            return -1;
+        }
+    }
+}
